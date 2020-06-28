b64054584fa9402af797b043dcf02fd5db7cca0e
Reloader <suarezfc82@gmail.com>
2020-06-27 21:29:05 -0500

    Initial Commit

---
+++ faceLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  faceLogin
+//
+//  Created by Resembrink Correa on 6/27/20.
+//  Copyright © 2020 Reloader. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
